d641713bc75634065cf0675b279ad8963d9a3ee7
jessiekrg <jessie.krg@gmail.com>
2025-04-21 10:57:22 +0200

    hihi

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-# Importation des modules nécessaires de Biopython
+#Importation des modules nécessaires de Biopython
 from Bio.PDB import PDBParser, PPBuilder
+import matplotlib.pyplot as plt
 
-# Étape 1 : Initialiser le parser pour lire le fichier PDB
+
+#Étape 1 : Initialiser le parser pour lire le fichier PDB
 parser = PDBParser(QUIET=True)  # QUIET=True pour éviter les avertissements lors du parsing
 
-# Étape 2 : Charger la structure depuis le fichier PDB
-# Remplacez "7cjs.pdb" par le nom de votre fichier PDB
-structure = parser.get_structure("structure_proteine", "7cjs.pdb")
+#Étape 2 : Charger la structure depuis le fichier PDB
+structure = parser.get_structure("structure_proteine", "3d9s.pdb")
 
-# Étape 3 : Initialiser le constructeur de polypeptides
+#Étape 3 : Initialiser le constructeur polypeptides
 ppb = PPBuilder()
+seq = []
 
-# Étape 4 : Parcourir les polypeptides construits et extraire les séquences
-for polypeptide in ppb.build_peptides(structure):
-    sequence = polypeptide.get_sequence()  # Obtient la séquence d'acides aminés
-    print(sequence)  # Affiche la séquence
+#Étape 4 : Parcourir les polypeptides construits et extraire les séquences
+for chaine in ppb.build_peptides(structure):
+    sequence = chaine.get_sequence()  #Obtient la séquence d'acides aminés d'une chaine
+    seq.append(sequence) # Ajoute une chaine à la liste séquence (4 : A,B,C,D)
 
+#Étape 5 : Echelle d'hydrophobicité
+Indice_H = {
+    'I':4.5, 
+}
+
+#Étape x : Calculer l'indice d'hydrophobicité de chaque aa
+def Calcul_Profil_H(Chaine):
+    return [Indice_H[aa] for aa in Chaine] # Retourne une liste de la valeur de chaque aa
+
+#Étape x : Maplotlib
